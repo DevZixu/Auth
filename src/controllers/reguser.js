7eb6dd4de571a345638bc2a875a97ea74a388264
Simon Bolaji <netforge27@gmail.com>
2026-09-17 22:15:07 +0100

    tested the existing in db check

--- src/controllers/reguser.js
+++ src/controllers/reguser.js
@@ -1,10 +1,10 @@
+const userDb = []
 
 export const regUser = (req, res) => {
     const {name, email, password, role} = req.body
-    const userDb = []
     const user = {
-        id : userDb.length + 1,
-        name : name,
+        id: userDb.length + 1,
+        name: name,
         email: email,
         password: password,
         role: role
@@ -12,12 +12,13 @@ export const regUser = (req, res) => {
     const existing = userDb.find(user => user.email === email);
     if (!existing) {
         userDb.push(user);
+        console.log(userDb);
         return res.status(200).json({
             status :  "created",
             message: "User has been registered"
         })
     };
-    return res.json({
+    return res.status(409).json({
         message: "user with this email already exists"
     });
 };
